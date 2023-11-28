7d4cb576836aaaa383901513ee42b71e7ff28b55
Mariia2006 <mariia.masenko1123@vu.cdu.edu.ua>
2023-11-28 22:42:18 +0200

    Перший коміт. Ініціалізація репозиторія

---
+++ Program1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char element = char.Parse(Console.ReadLine());
+            if ((int)element >= 65 && (int)element <= 90)
+            {
+                Console.WriteLine(element.ToString().ToLower());
+            }
+            if ((int)element >= 97 && (int)element <= 122)
+            {
+                Console.WriteLine(element.ToString().ToUpper());
+            }
+        }
+    }
+}
