d11ba5befd4d771495bea351dd2e21b41ecf21d1
Mariia2006 <145233153+Mariia2006@users.noreply.github.com>
2023-11-29 22:24:11 +0200

    Create Program4.cs

---
+++ Program4.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+            int counter = 0;
+            foreach (char c in line)
+            {
+                counter++;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
